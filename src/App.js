644d98f561e47f18d47c2a7c81a87f3b89508de9
Satyam-Rai-1 <satyam154gzp@gmail.com>
2023-08-20 12:55:26 +0530

    Starting project, adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Error from './Error';
+import Products from './Products';
+import Contact from './Contact';
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element= {<About/>}/>
+      <Route path='/products' element= {<Products/>}/>
+      <Route path='/contact' element= {<Contact/>}/>
+      <Route path='/singleproduct/:id' element= {<SingleProduct/>}/>
+      <Route path='/cart' element= {<Cart/>}/>
+      <Route path='*' element={<Error/>}/>
+    </Routes>
+  
+  </BrowserRouter>  
+    
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className=''>
+
+      Error
+    </div>
+  )
+}
+
+export default Error
