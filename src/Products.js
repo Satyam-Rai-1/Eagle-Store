1eea21d86634bac767c04fd3e5123dfd28da854e
Satyam-Rai-1 <satyam154gzp@gmail.com>
2023-08-28 19:34:16 +0530

    update products page

--- src/Products.js
+++ src/Products.js
@@ -1,8 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import FilterSection from "./components/FilterSection";
+import Sort from "./components/Sort";
+import ProductList from "./components/ProductList";
+import { useFilterContext } from "./context/filter_context";
+
+
+
+
 
 const Products = () => {
-  return <Wrapper></Wrapper>;
+
+
+  const {filter_products} = useFilterContext();
+  // console.log(
+  //   "~file : products.js ~ line 16 ~ products ~ filter_products",
+  //   filter_products
+  // );
+  return <Wrapper>
+    <div className="container grid grid-filter-column">
+      <div>
+        <FilterSection/>
+      </div>
+
+      <section className="product-view--sort">
+
+        <div className="sort-filter">
+          <Sort/>
+        </div>
+
+        <div className="main-product">
+          <ProductList/>
+        </div>
+
+      </section>
+    </div>
+  </Wrapper>;
 };
 
 const Wrapper = styled.section`
--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -9,6 +9,7 @@ import FormatPrice from "./helper/FormatPrice";
 import { TbTruckDelivery, TbReplace } from "react-icons/tb";
 import { MdSecurity } from "react-icons/md";
 import Star from "./components/Star";
+import AddToCart from "./components/AddToCart";
 
 
 const API = "https://api.pujakaitem.com/api/products";
@@ -105,8 +106,8 @@ const SingleProduct = () => {
               
             </div>
 
-
-
+            <hr />
+            {stock > 0 && <AddToCart product={singleProduct}/>}
             
           </data>
         </div>
